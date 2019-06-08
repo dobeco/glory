3d26ce4d1d16e5ece7ff93720ba30a49f0e5f32a
dobeco <736278325@qq.com>
2019-06-08 23:56:20 +0800

    添加主题、rem适配

--- web/src/main.js
+++ web/src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import './assets/iconfont/iconfont.css'
+import './assets/scss/style.scss'
 
 
 Vue.config.productionTip = false
