b98140afa57e8d1c17922ff52890b35d9d605539
dobeco <736278325@qq.com>
2019-08-20 23:21:49 +0800

    完善新闻资讯

--- web/src/components/ListCard.vue
+++ web/src/components/ListCard.vue
@@ -1,14 +1,18 @@
 <template>
   <m-card :icon="icon" :title="title">
     <div class="nav jc-between">
-      <div class="nav-item" :class="{active: active === i}"
-      v-for="(category, i) in categories" :key="i"
-      @click="active = i">
+      <div
+        class="nav-item"
+        :class="{active: active === i}"
+        v-for="(category, i) in categories"
+        :key="i"
+        @click="$refs.list.swiper.slideTo(i)"
+      >
         <div class="nav-link">{{category.name}}</div>
       </div>
     </div>
     <div class="pt-3">
-      <swiper>
+      <swiper ref="list" @slide-change="()=> active = $refs.list.swiper.realIndex">
         <swiper-slide v-for="(category, i) in categories" :key="i">
           <slot name="items" :category="category"></slot>
         </swiper-slide>
@@ -24,7 +28,7 @@ export default {
     title: { type: String, required: true },
     categories: { type: Array, required: true }
   },
-  data(){
+  data() {
     return {
       active: 0
     }
